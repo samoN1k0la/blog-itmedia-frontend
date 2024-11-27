289cca392e67b7674949221d7d615dfe1291565f
samoN1k0la <nikola400lukic@gmail.com>
2024-11-27 12:25:26 +0100

    Admin panel - user management tabular view

--- src/pages/Admin/AdminPanel.tsx
+++ src/pages/Admin/AdminPanel.tsx
@@ -19,18 +19,39 @@ const AdminPanel: React.FC = () => {
             </ul>
           </div>
         );
-      case "Authors":
-        return (
-          <div>
-            <h1>Author Management</h1>
-            <p>View and manage all authors on the platform.</p>
-            <ul>
-              <li>Author A (Posts: 10, Status: Active)</li>
-              <li>Author B (Posts: 5, Status: Suspended)</li>
-              <li>Author C (Posts: 20, Status: Active)</li>
-            </ul>
-          </div>
-        );
+        case "Authors":
+          return (
+            <div className="author-management">
+              <h1>Author Management</h1>
+              <p>View and manage all authors on the platform.</p>
+              <table className="author-table">
+                <thead>
+                  <tr>
+                    <th>Author Name</th>
+                    <th>Posts</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Author A</td>
+                    <td>10</td>
+                    <td>Active</td>
+                  </tr>
+                  <tr>
+                    <td>Author B</td>
+                    <td>5</td>
+                    <td>Suspended</td>
+                  </tr>
+                  <tr>
+                    <td>Author C</td>
+                    <td>20</td>
+                    <td>Active</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          );        
       case "Moderation":
         return (
           <div>
