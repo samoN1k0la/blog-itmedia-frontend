c58eab1bdd27996b7bfea96c021aa200fece239e
samoN1k0la <nikola400lukic@gmail.com>
2024-12-06 23:33:28 +0100

    Minor design improvements

--- src/pages/Author/AuthorPanel.tsx
+++ src/pages/Author/AuthorPanel.tsx
@@ -3,10 +3,11 @@ import "./AuthorPanel.css";
 
 const AuthorPanel: React.FC = () => {
     return (
-        <div className="author-panel">
+        <div className="author-panel-container">
             <div className="author-panel-content">
-                <h2>Page Under Development</h2>
-                <p>Our developers are currently working hard to bring you this feature. Please check back soon!</p>
+                <h1 className="author-panel-title">Coming Soon</h1>
+                <p className="author-panel-message">Our developers are currently working hard to bring you this feature. Please check back soon!</p>
+                <a href="/" className="author-panel-button">Go Back Home</a>
             </div>
         </div>
     );
